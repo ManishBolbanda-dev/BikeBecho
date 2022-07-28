1dcb95cc70eb0f5fd50e4442063774458811d7cc
Manish Bolbanda <73099567+ManishBolbanda-dev@users.noreply.github.com>
2022-07-28 22:36:52 +0530

    Add files via upload

---
+++ src/com/BikeBecho/Utility/DBUtility.java
@@ -0,0 +1,24 @@
+package com.BikeBecho.Utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtility
+{
+	public static Connection connection()
+	{
+		Connection con=null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost/BikeBecho", "root", "root");
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
